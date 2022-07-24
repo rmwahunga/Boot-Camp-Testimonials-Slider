7028fd375c874a7b25ac25da6174f84dcc1886e1
rmwahunga <shoboirichard@gmail.com>
2022-07-24 20:11:01 +0300

    first commit

---
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -0,0 +1,29 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+
+function nextSlide () {
+    index++;
+    if (index > slides.length -1) {
+        index = 0;
+    }
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';    
+}
+function prevSlide () {
+    index--;
+    if (index < 0) {
+        index = slides.length-1;
+    }
+    slides.forEach((slide) => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex';    
+}
+
+next.addEventListener('click', nextSlide);
+prev.add
